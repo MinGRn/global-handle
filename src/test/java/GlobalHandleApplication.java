7f60d7b5e7e876bbef5b5df062bb1b6fa30cf036
MinGRn <MinGRn97@gmail.com>
2018-11-08 21:07:41 +0800

    修改测试主类;

--- src/test/java/GlobalHandleApplication.java
+++ src/test/java/GlobalHandleApplication.java
@@ -1,9 +1,8 @@
-import com.mingrn.common.global.annotation.ParamsCheck;
+import com.mingrn.common.global.annotation.Checked;
 import com.mingrn.common.global.annotation.ParamsIsNotNull;
 import com.mingrn.common.global.exception.NotLoginException;
 import com.mingrn.common.global.result.ResponseMsgUtil;
 import com.mingrn.common.global.result.Result;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping
@@ -22,11 +24,34 @@ public class GlobalHandleApplication {
 	}
 
 
-	@ParamsCheck
-	@GetMapping("/notLoginException")
-	public Result notLoginException(@ParamsIsNotNull @RequestParam String name,
-									@RequestParam(required = false) String value) {
-		return ResponseMsgUtil.success("这是成功请求示例");
+	@Checked
+	@GetMapping("/notLoginException1")
+	public Result notLoginException1(@ParamsIsNotNull @RequestParam String name,
+									 @RequestParam(required = false) String value) {
+		HashMap<String, Object> map = null;
+		return ResponseMsgUtil.success(map);
+	}
+
+	@Checked
+	@GetMapping("/notLoginException2")
+	public Result notLoginException2(@ParamsIsNotNull @RequestParam String name,
+									 @RequestParam(required = false) String value) {
+		return ResponseMsgUtil.success(null);
+	}
+
+	@Checked
+	@GetMapping("/notLoginException3")
+	public Result notLoginException3(@ParamsIsNotNull @RequestParam String name,
+									 @RequestParam(required = false) String value) {
+		ArrayList<String> list = null;
+		return ResponseMsgUtil.success(list);
+	}
+
+	@Checked
+	@GetMapping("/notLoginException4")
+	public Result notLoginException4(@ParamsIsNotNull @RequestParam String name,
+									 @RequestParam(required = false) String value) {
+		return ResponseMsgUtil.success("");
 	}
 
 	@GetMapping("/success")
