152599752cca1ba445f019222e1f343c28d176ba
MinGRn <MinGRn97@gmail.com>
2018-11-08 20:58:39 +0800

    RequestUtil 工具类修改注释;

--- src/main/java/com/mingrn/common/global/util/RequestUtil.java
+++ src/main/java/com/mingrn/common/global/util/RequestUtil.java
@@ -6,15 +6,19 @@ import javax.servlet.http.HttpServletRequest;
  * @author MinGRn <br > MinGRn97@gmail.com
  */
 public class RequestUtil {
+
+	private RequestUtil() {
+	}
+
 	private static final String UNKNOWN_IP = "unknown";
 
 	/**
 	 * 获取用户真实IP地址，不使用request.getRemoteAddr();的原因是有可能用户使用了代理软件方式避免真实IP地址,
 	 * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值，究竟哪个才是真正的用户端的真实IP呢？
 	 * 答案是取X-Forwarded-For中第一个非unknown的有效IP字符串。
-	 * 如：X-Forwarded-For：192.168.1.110, 192.168.1.120, 192.168.1.130,
+	 * 如:X-Forwarded-For:192.168.1.110, 192.168.1.120, 192.168.1.130,
 	 * 192.168.1.100
-	 * 用户真实IP为： 192.168.1.110
+	 * 用户真实IP为: 192.168.1.110
 	 */
 	public static String getIpAddress(HttpServletRequest request) {
 		String ip = request.getHeader("x-forwarded-for");
