948280edefd7ee271124de357f1e13bcb7f3e250
MinGRn <MinGRn97@gmail.com>
2018-11-08 09:24:13 +0800

    增加描述

--- src/main/java/com/mingrn/common/global/annotation/ParamsCheck.java
+++ src/main/java/com/mingrn/common/global/annotation/ParamsCheck.java
@@ -2,6 +2,12 @@ package com.mingrn.common.global.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * 参数检查注解,用于开启 {@link ParamsIsNotNull} 注解功能,作用于方法
+ *
+ * @author MinGRn <br > MinGRn97@gmail.com
+ * @date 08/11/2018 09:22
+ */
 @Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
