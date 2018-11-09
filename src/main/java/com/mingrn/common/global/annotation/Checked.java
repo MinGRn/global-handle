26d132a65246d1fb8ab3ce2a9700d05ccb634a65
MinGRn <MinGRn97@gmail.com>
2018-11-09 20:10:56 +0800

    未做功能操作;

--- src/main/java/com/mingrn/common/global/annotation/Checked.java
+++ src/main/java/com/mingrn/common/global/annotation/Checked.java
@@ -3,7 +3,7 @@ package com.mingrn.common.global.annotation;
 import java.lang.annotation.*;
 
 /**
- * 参数检查注解,用于开启 {@link ParamsIsNotNull} 注解功能,作用于方法
+ * 检查注解,作用于方法
  *
  * @author MinGRn <br > MinGRn97@gmail.com
  * @date 08/11/2018 09:22
