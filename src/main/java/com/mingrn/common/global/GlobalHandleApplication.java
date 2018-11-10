91627155c87b41890c60d8ffcf42140328f13ed5
MinGRn <MinGRn97@gmail.com>
2018-11-10 13:07:26 +0800

    修改:启动类也测试示例;

---
+++ src/main/java/com/mingrn/common/global/GlobalHandleApplication.java
@@ -0,0 +1,71 @@
+package com.mingrn.common.global;
+
+import com.mingrn.common.global.annotation.Checked;
+import com.mingrn.common.global.annotation.ParamsIsNotNull;
+import com.mingrn.common.global.exception.NoOperateAuthorityException;
+import com.mingrn.common.global.exception.NotLoginException;
+import com.mingrn.common.global.result.ResponseMsgUtil;
+import com.mingrn.common.global.result.Result;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping
+@SpringBootApplication
+public class GlobalHandleApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(GlobalHandleApplication.class).web(WebApplicationType.SERVLET).run(args);
+	}
+
+
+	@Checked
+	@GetMapping("/notLoginException1")
+	public Result notLoginException1(@ParamsIsNotNull @RequestParam String name,
+									 @RequestParam(required = false) String value) {
+		HashMap<String, Object> map = null;
+		return ResponseMsgUtil.success(map);
+	}
+
+	@Checked
+	@GetMapping("/notLoginException2")
+	public Result notLoginException2(@RequestParam String name,
+									 @RequestParam(required = false) String value) {
+		notLoginExceptionTest();
+		return ResponseMsgUtil.success(null);
+	}
+
+	@Checked
+	@GetMapping("/notLoginException3")
+	public Result notLoginException3(@ParamsIsNotNull @RequestParam String name,
+									 @RequestParam(required = false) String value) {
+		ArrayList<String> list = null;
+		return ResponseMsgUtil.success(list);
+	}
+
+	@Checked
+	@GetMapping("/notLoginException4")
+	public Result notLoginException4(@ParamsIsNotNull @RequestParam String name,
+									 @RequestParam(required = false) String value) {
+		return ResponseMsgUtil.success("");
+	}
+
+	@GetMapping("/success")
+	public Result success() {
+		return ResponseMsgUtil.success("这是成功请求示例");
+	}
+
+
+	private void notLoginExceptionTest() {
+		throw new NoOperateAuthorityException();
+	}
+
+}
--- src/test/java/GlobalHandleApplication.java
+++ src/test/java/GlobalHandleApplication.java
@@ -1,67 +1,23 @@
-import com.mingrn.common.global.annotation.Checked;
-import com.mingrn.common.global.annotation.ParamsIsNotNull;
-import com.mingrn.common.global.exception.NotLoginException;
-import com.mingrn.common.global.result.ResponseMsgUtil;
-import com.mingrn.common.global.result.Result;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-@RestController
-@RequestMapping
-@SpringBootApplication
 public class GlobalHandleApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(GlobalHandleApplication.class).web(WebApplicationType.SERVLET).run(args);
+
+//		final String regex = "[^\\[]+(?=])";
+//		final String regex = "[^\\[\\s]+(?=])";
+		final String regex = "(?<=\\[(?:class )?)[^\\]]+";
+		final String string = "request [class java.lang.String] parameter [name] must not be empty or null \n";
+
+		final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+		final Matcher matcher = pattern.matcher(string);
+
+		while (matcher.find()) {
+			System.out.println("Full match: " + matcher.group(0));
+			for (int i = 1; i <= matcher.groupCount(); i++) {
+				System.out.println("Group " + i + ": " + matcher.group(i));
+			}
+		}
 	}
-
-
-	@Checked
-	@GetMapping("/notLoginException1")
-	public Result notLoginException1(@ParamsIsNotNull @RequestParam String name,
-									 @RequestParam(required = false) String value) {
-		HashMap<String, Object> map = null;
-		return ResponseMsgUtil.success(map);
-	}
-
-	@Checked
-	@GetMapping("/notLoginException2")
-	public Result notLoginException2(@ParamsIsNotNull @RequestParam String name,
-									 @RequestParam(required = false) String value) {
-		return ResponseMsgUtil.success(null);
-	}
-
-	@Checked
-	@GetMapping("/notLoginException3")
-	public Result notLoginException3(@ParamsIsNotNull @RequestParam String name,
-									 @RequestParam(required = false) String value) {
-		ArrayList<String> list = null;
-		return ResponseMsgUtil.success(list);
-	}
-
-	@Checked
-	@GetMapping("/notLoginException4")
-	public Result notLoginException4(@ParamsIsNotNull @RequestParam String name,
-									 @RequestParam(required = false) String value) {
-		return ResponseMsgUtil.success("");
-	}
-
-	@GetMapping("/success")
-	public Result success() {
-		return ResponseMsgUtil.success("这是成功请求示例");
-	}
-
-
-	private void notLoginExceptionTest() {
-		throw new NotLoginException();
-	}
-
 }
