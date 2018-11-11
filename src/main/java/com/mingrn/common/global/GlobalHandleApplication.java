a439354be19c3b6570226f73019b1324cc495949
MinGRn <MinGRn97@gmail.com>
2018-11-11 15:47:12 +0800

    新增:swagger2 config、测试用例;

--- src/main/java/com/mingrn/common/global/GlobalHandleApplication.java
+++ src/main/java/com/mingrn/common/global/GlobalHandleApplication.java
@@ -3,9 +3,12 @@ package com.mingrn.common.global;
 import com.mingrn.common.global.annotation.Checked;
 import com.mingrn.common.global.annotation.ParamsIsNotNull;
 import com.mingrn.common.global.exception.NoOperateAuthorityException;
-import com.mingrn.common.global.exception.NotLoginException;
 import com.mingrn.common.global.result.ResponseMsgUtil;
 import com.mingrn.common.global.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -16,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
+@Api(description = "Swagger API")
 @RestController
 @RequestMapping
 @SpringBootApplication
@@ -29,6 +32,11 @@ public class GlobalHandleApplication {
 
 	@Checked
 	@GetMapping("/notLoginException1")
+	@ApiOperation(value = "未登录异常",notes = "未登录异常测试")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "name",value = "名称",paramType = "query",dataType = "String",required = true),
+			@ApiImplicitParam(name = "value",value = "值",paramType = "query",dataType = "String",required = true)
+	})
 	public Result notLoginException1(@ParamsIsNotNull @RequestParam String name,
 									 @RequestParam(required = false) String value) {
 		HashMap<String, Object> map = null;
